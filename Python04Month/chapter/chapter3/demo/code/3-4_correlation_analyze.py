9e3625d79b9f64576d417be14b9b3f60797aacdd
liujing <15201078137@163.com>
2018-04-23 16:43:48 +0800

    2
    2

--- Python04Month/chapter/chapter3/demo/code/3-4_correlation_analyze.py
+++ Python04Month/chapter/chapter3/demo/code/3-4_correlation_analyze.py
@@ -8,7 +8,9 @@ d1 = data = pd.read_excel(catering_sale, index_col = u'日期') #读取数据，
 print(d1)
 d2 = data.corr() #相关系数矩阵，即给出了任意两款菜式之间的相关系数
 print(d2)
+d2.to_excel('../tmp/xishu01.xls')
 d3 = data.corr()[u'百合酱蒸凤爪'] #只显示“百合酱蒸凤爪”与其他菜式的相关系数
-print(d3)
+print(u'd3 "%s' % d3)
+d3.to_excel('../tmp/xishu02.xls')
 d4 = data[u'百合酱蒸凤爪'].corr(data[u'翡翠蒸香茜饺']) #计算“百合酱蒸凤爪”与“翡翠蒸香茜饺”的相关系数
-print(d4)
+# print(u'd4 "%s"' % d4)
